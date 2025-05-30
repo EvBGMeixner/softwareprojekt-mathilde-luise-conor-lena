512317c469642c17810a488ebd66ed8efea8a82b
MathildeBuchholz <mathilde.buchholz@student.evbg.de>
2025-05-30 16:09:22 +0200

    m

---
+++ Anfang.java
@@ -0,0 +1,18 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Anfang.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Anfang extends SPIEL
+{
+    
+    public Anfang()
+    {
+        
+    }
+    
+
+    
+}
--- HüpfUndRenne.java
+++ HüpfUndRenne.java
@@ -1,9 +1,11 @@
 
 
-public class HüpfUndRenne
+public class HüpfUndRenne extends SPIEL
 {
     SPIEL spiel;
     Level1 level;
+    //FIGUR spielfigur;
+    KREIS spielfigur;
     /**
      * Konstruktor für Objekte der Klasse HüpfUndRenne
      */
@@ -11,6 +13,15 @@ public class HüpfUndRenne
     {
         spiel= new SPIEL(1100, 700, true);
         //level=new Level1();
+        //spielfigur = new FIGUR("muss noch ein bild eingefügt werden");
+        spielfigur=new KREIS(0.3);
+    }
+    //@Override
+    public void tasteReagieren( int taste ) 
+    {
+        if(taste==39){
+            spielfigur.verschieben(0, 1);
+        }
     }
 
     
