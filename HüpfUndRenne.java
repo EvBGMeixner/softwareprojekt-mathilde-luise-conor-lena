dac22a76d4458cfad7765f51d0ee35be50fb62c4
MathildeBuchholz <mathilde.buchholz@student.evbg.de>
2025-05-30 14:45:45 +0200

    a

--- HüpfUndRenne.java
+++ HüpfUndRenne.java
@@ -3,12 +3,14 @@
 public class HüpfUndRenne
 {
     SPIEL spiel;
+    Level1 level;
     /**
      * Konstruktor für Objekte der Klasse HüpfUndRenne
      */
     public HüpfUndRenne()
     {
         spiel= new SPIEL(1100, 700, true);
+        //level=new Level1();
     }
 
     
--- Level1.java
+++ Level1.java
@@ -2,10 +2,10 @@
 
 public class Level1 extends LEVEL
 {
-    Level1 level;
+    
     public Level1()
     {
         KREIS kreis= new KREIS();
-        level= new Level1();
+                
     }
 }
