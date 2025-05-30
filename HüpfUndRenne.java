04942c2968160d0cf7ffe6e1784cb5e526552144
MathildeBuchholz <mathilde.buchholz@student.evbg.de>
2025-05-30 17:33:59 +0200

    m

--- HüpfUndRenne.java
+++ HüpfUndRenne.java
@@ -6,22 +6,50 @@ public class HüpfUndRenne extends SPIEL
     Level1 level;
     //FIGUR spielfigur;
     KREIS spielfigur;
+    RECHTECK boden;
     /**
      * Konstruktor für Objekte der Klasse HüpfUndRenne
      */
     public HüpfUndRenne()
     {
-        spiel= new SPIEL(1100, 700, true);
+        //spiel= new SPIEL(1100, 700, true);
         //level=new Level1();
         //spielfigur = new FIGUR("muss noch ein bild eingefügt werden");
+        boden=new RECHTECK(40,1);
+        boden.setzeMittelpunkt(0, -5);
+        boden.machePassiv();
         spielfigur=new KREIS(0.3);
+        spielfigur.macheAktiv();
+        //starteTickerNeu(0.02);
+        //bildAktualisierungReagieren(0.02);
     }
-    //@Override
+    public void tick(){
+        if(istTasteGedrueckt(39)==true){
+             spielfigur.verschieben(0.1, 0);
+        }
+        if(istTasteGedrueckt(37)==true){
+            spielfigur.verschieben(-0.1, 0);
+        }
+    }
+    public void bildAktualisierungReagieren(){
+        if(istTasteGedrueckt(39)==true){
+             spielfigur.verschieben(0.1, 0);
+        }
+        if(istTasteGedrueckt(37)==true){
+            spielfigur.verschieben(-0.1, 0);
+        }
+        
+    }
     public void tasteReagieren( int taste ) 
     {
-        if(taste==39){
-            spielfigur.verschieben(0, 1);
+        if(taste==38){
+            spielfigur.springe(7);
+            
         }
+        
+        
+        
+        
     }
 
     
