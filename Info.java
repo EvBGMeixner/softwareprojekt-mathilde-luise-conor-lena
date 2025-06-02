130ca72d42a421675e5b9ebf0d493fb081f6b0ca
MathildeBuchholz <mathilde.buchholz@student.evbg.de>
2025-06-02 21:09:59 +0200

    m

---
+++ Info.java
@@ -0,0 +1,14 @@
+
+public class Info
+{
+    TEXT text;
+    int x;
+    int y;
+    String inhalt;
+    public Info(int _x, int _y, String _inhalt)
+    {
+        x=_x;
+        y=_y;
+        inhalt=_inhalt;
+    }
+}
