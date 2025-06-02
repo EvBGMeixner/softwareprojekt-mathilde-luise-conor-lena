4280fe9bc98a12fad652659036e9b691f0f497fc
MathildeBuchholz <mathilde.buchholz@student.evbg.de>
2025-06-02 21:21:29 +0200

    info

--- INFO.java
+++ INFO.java
@@ -1,15 +1,17 @@
 
-public class Info
+public class INFO
 {
     TEXT text;
+    RECHTECK rechteck;
     int x;
     int y;
     String inhalt;
-    public Info(int _x, int _y, String _inhalt)
+    public INFO(int _x, int _y, String _inhalt)
     {
         x=_x;
         y=_y;
         inhalt=_inhalt;
+        rechteck=new RECHTECK(1,1);
     }
     public void textAnzeigen(){
         if(text==null){
